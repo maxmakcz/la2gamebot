1f08952d1955aed705b80e40678bd2ab165bdad5
maxmakcz <112479447+maxmakcz@users.noreply.github.com>
2022-08-31 13:36:25 +0300

    Update botl2.py

--- botl2.py
+++ botl2.py
@@ -63,10 +63,11 @@ async def start(message: types.Message):
         cur.execute(f'INSERT INTO players(id ,nick , zat ,Krit ,def  ,atk  ,atkxp ,hp  ,hpxp  ,'
                     f'xp  ,RB1  ,RB2  ,RB3  , tochkab , tochkam , tochkar , minirb  , mob1 ,mob2  , mob3 , minirb2 , Weapon , dion ,'
                     f' giran , aden ,mob4 , mob5, mob6 , minirb3 , mob7 , mob8 , mob9 ,mob10 , armor , gold , defxp ,'
-                    f' maxhp , zat2 ,profa ,buff ,srec , Varnish ,rbfight ,mp ,maxmp ,hpbanka ,mpbanka ,mob11 ,mob12 ,dinoq ,nosq ,aliq ,bufq ,lionq ,bearq ,mobr ,mobra)  VALUES (%s,%s,0,1,1,5,0,140,0,0,10000'
-                          f',50000,150000,0,0,0,0,600,2200,12000,200000,'
+                    f' maxhp , zat2 ,profa ,buff ,srec , Varnish ,rbfight ,mp ,maxmp ,hpbanka ,mpbanka ,mob11 ,mob12 ,dinoq ,
+                    f' nosq ,aliq ,bufq ,lionq ,bearq ,mobr ,mobra)  VALUES (%s,%s,0,1,1,5,0,140,0,0,10000,'
+                          f'50000,150000,0,0,0,0,600,2200,12000,200000,'
                           f'%s,0,0,0,2000,3600,5000,400000,6400,10000,11000,13000,'
-                          f'%s,0,0,100,0,%s,0,%s,16000,16000,0,0,0,0,0,0,0,0)',(user_id,nick,df,bs,rec,rec2))
+                          f'%s,0,0,100,0,%s,0,%s,16000,16000,0,0,0,0,0,0,0,0,0,0,0,0,0,0)',(user_id,nick,df,bs,rec,rec2))
         con.commit()
     else:
         await message.answer('Вы уже зарегистрированы')
